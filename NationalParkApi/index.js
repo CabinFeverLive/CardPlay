b3f9ee7941bb1dbacd18bb50483f350e7cd99859
Randy Preval <mrpreval@gmail.com>
2019-11-24 10:15:40 -0800

    updated endpoint within Js

--- NationalParkApi/index.js
+++ NationalParkApi/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const apiKey = 'MZmerdcgQ4JnrzQ0cS0MIrQhFsuHx6d1LT0rsIjW';
-const searchUrl = 'https://developer.nps.gov/api/v1/parks';
+const searchUrl = 'https://developer.nps.gov/api/v1/places';
 
 function formatQueryParams(params){
     const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
@@ -11,23 +11,24 @@ function formatQueryParams(params){
 function displayResults(responseJson){
   let resultsArray = responseJson.data
   let html =''
-    // console.log(resultsArray)
+    console.log(resultsArray)
   for (const repo of resultsArray){
       // console.log(repo)
-      if(responseJson.length < 1){
+      if(responseJson.total == 0){
+        
         html += `Sorry, that park cannot be found, please try again!`
       }
       else{
         html += `<ul>
         <li>
-            <h3>${repo.fullName}</h3>
-            <p>${repo.description}</p>
+            <h3>${repo.title}</h3>
+            <p>${repo.listingdescription}</p>
             <p>${repo.url}</p>
         </li>
         </ul>
       `
       }
-      
+      console.log(responseJson.total)
   }
   $('#searchResults').html(html)
 
