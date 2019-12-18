7babd513ead16df9d5873ac854969240e241216c
Randy Preval <mrpreval@gmail.com>
2019-12-18 12:40:00 -0800

    revisions complete

--- Portfolio2/index.js
+++ Portfolio2/index.js
@@ -5,7 +5,6 @@ $( document ).ready(function() {
     $('#showButtonLeft').toggleClass('active');
     $('#showButtonRight').toggleClass('active');
   $("#showButtonLeft").click(function() {
-    console.log('clicked left section');
     $('.page-inner').show();
   	$('#sec-left').width('100%');
     $('#sec-right').hide();
@@ -16,7 +15,6 @@ $( document ).ready(function() {
   });
   
   $('#backButtonLeft').click(function(){
-    console.log('sections back to half-half');
     $('.page-inner').hide();
   	$("#sec-left").width("50%");
     $('#sec-right').show();
@@ -27,7 +25,6 @@ $( document ).ready(function() {
   });
   
   $("#showButtonRight").click(function() {
-    console.log('clicked right section');
     $('.page-inner').show();
   	$('#sec-right').width('100%');  
     $('#sec-left').hide();
@@ -38,7 +35,6 @@ $( document ).ready(function() {
   });
   
   $('#backButtonRight').click(function(){
-    console.log('sections back to half-half');
     $('.page-inner').hide();
   	$('#sec-right').width('50%');  
     $('#sec-left').show();
@@ -52,107 +48,3 @@ $( document ).ready(function() {
 
 
 
-/* $( document ).ready(function() {
-  // Handler for .ready() called.
-      $(".section1").click(function() {
-        $('.page-left').css({transform:'translateX(0%)'})
-        $('.section1').css({width:'100%'})
-        // $('.page-left').removeAttr('hidden')
-        $('.section2').hide()
-        $('.page-left').show()
-      });
-
-
-    // $('.backButtonLeft').click(function(){
-    //     // $('.section1').css({width:'50%'})
-    //     // $('.section2').css({width:'50%'})
-
-    //     // // $('.section1,.section2').css({width:'50%'})
-    //     // // $('.section1').css({width:'50%'})
-    //     // $('.section2').show()
-    //     // $('.page-left').attr(hidden='true')
-    //     // $('.page-right').removeAttr('hidden')
-    //     // // $('.page-left').hide()
-
-    //     // $('.page-left').css({transform:'translateX(-100%)'})
-    //     // $('.section1').css({width:'50%'})
-    //     // // $('.page-left').removeAttr('hidden')
-    //     // $('.section2').show()
-    //     // $('.section2').css({display:'flex'})
-    //     // $('.page-left').hide()
-
-    //     $('.page-left').css({transform:'translateX(-100%)'})
-    //     $('.section1').css({width:'50%'})
-    //     // $('.page-left').removeAttr('hidden')
-    //     $('.page-left').hide()
-    //     $('.section2').show()
-
-    //     console.log('the back button works')
-    // });
-});
-
-
-
-  function sideClick(){
-    // $(".section1").click(function() {
-    //     $('.page-left').css({transform:'translateX(0%)'})
-    //     $('.section1').css({width:'100%'})
-    //     $('.section2').hide()
-    //     $('.page-left').removeAttr('hidden')
-    //   });
-
-      
-
-      $(".section2").click(function() {
-        $('.page-right').css({transform:'translateX(0%)'})
-
-        $('.page-right').css({zIndex: '100'})
-
-        $('.section1').hide()
-
-        $('.container').css({gridTemplateColumns:'100%'})
-
-        $('.container').removeClass('.section1')
-
-        
-      });
-
-       $('.backButtonLeft').click(function(){
-        // $('.section1').css({width:'50%'})
-        $(".section1").width("50%");
-        $('.section2').show()
-        $('.page-left').attr('hidden')
-          // $('.section2').show()
-          // $('.page-left').hide()
-
-          // console.log()
-      });
-
-      $('.backButtonRight').click(function(){
-
-    });
-  }
-
-  // function backClick(){
-      // $('.backButtonLeft').click(function(){
-      //     $('.page-left').css({transform:'translateX(100%)'})
-
-      //     $('.container').css({gridTemplateColumns:'1fr 1fr'})
-      //     console.log('the back button works')
-      // });
-
-    $('.backButtonLeft').click(function(){
-        $('.page-left').css({transform:'translateX(-100%)'})
-        $('.section1').css({width:'50%'})
-        $('.page-left').hide()
-        $('.section2').show()
-        console.log('the back button works')
-    });      
-
-      $('.backButtonRight').click(function(){
-
-    });
-  // }
-
-  sideClick()
-  */
