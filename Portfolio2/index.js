b15414dd5d816d5b419e7e774113874726e3d2ef
Jemel Preval <cabinfeverchronicles@gmail.com>
2020-04-17 19:22:49 -0700

    added typed js woohoo!

--- Portfolio2/index.js
+++ Portfolio2/index.js
@@ -44,16 +44,42 @@ $( document ).ready(function() {
     $('#backButtonRight').hide();
   });  
   
-  // function renderLogo(){
-  //   fetch("logo.json")
-  //     .then(function(response){
-  //         return response.json();
-  //     })
-  //     .then(function(logo){
-  //       console.log(logo)
-      
-  //     })
-  //   }
+  $(function(){
+    $(".typed").typed({
+      strings: ["drawing I'm Wireframing", "wireframing I'm importing psd files.", "importing, I'm npx creating a React app."],
+      // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+      stringsElement: null,
+      // typing speed
+      typeSpeed: 30,
+      // time before typing starts
+      startDelay: 1200,
+      // backspacing speed
+      backSpeed: 20,
+      // time before backspacing
+      backDelay: 500,
+      // loop
+      loop: true,
+      // false = infinite
+      loopCount: 5,
+      // show cursor
+      showCursor: false,
+      // character for cursor
+      cursorChar: "|",
+      // attribute to type (null == text)
+      attr: null,
+      // either html or text
+      contentType: 'html',
+      // call when done callback function
+      callback: function() {},
+      // starting callback function before each string
+      preStringTyped: function() {},
+      //callback for every typed string
+      onStringTyped: function() {},
+      // callback for reset
+      resetCallback: function() {}
+    });
+  });
+  
   
 
 });
