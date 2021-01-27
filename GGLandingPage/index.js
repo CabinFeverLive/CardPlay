3b88a8bc249b926535aace44dd35b04198a8ade1
Jemel Preval <cabinfeverchronicles@gmail.com>
2021-01-26 19:05:55 -0800

    mad changes from font fam and spacing to jquery

---
+++ GGLandingPage/index.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    $("#").click(function() {
+        //unhide spit gif, possibly set timeout for unhide gif
+        //unhide or move on to next question, hide current question on choice and show next questions
+        //onclick function for next set of buttons, choice unhides gif
+      });
+
+    $("#").click(function() {
+        //unhide knock gif, possibly set timeout for unhide gif
+        //unhide or move on to next question, hide current question on choice and show next questions
+        //onclick function for next set of buttons, choice unhides gif
+       });
+});
