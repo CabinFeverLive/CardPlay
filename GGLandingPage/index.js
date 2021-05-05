ee1639f56d743565bc5f145c29081e530fe3a986
Randy Preval <mrpreval@gmail.com>
2021-05-05 10:49:15 -0700

    new js and css but css needs flex!

--- GGLandingPage/index.js
+++ GGLandingPage/index.js
@@ -1,11 +1,15 @@
 $( document ).ready(function() {
-    $("#").click(function() {
+    $(".choice_one").click(function() {
+      // console.log('yea boyyyyee')
+      $(".overlay_girl_spit").css({"display":"block","visibility":"visible"})
+      $(".overlay_boy_spit").css({"display":"block","visibility":"visible"})
+      
         //unhide spit gif, possibly set timeout for unhide gif
         //unhide or move on to next question, hide current question on choice and show next questions
         //onclick function for next set of buttons, choice unhides gif
       });
 
-    $("#").click(function() {
+    $(".choice_two").click(function() {
         //unhide knock gif, possibly set timeout for unhide gif
         //unhide or move on to next question, hide current question on choice and show next questions
         //onclick function for next set of buttons, choice unhides gif
